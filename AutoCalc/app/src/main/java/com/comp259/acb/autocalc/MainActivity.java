7d8548fa74ed793e0ffb20fec7b3a9262e986c3b
Amanda Braun <Ac_br_aun@hotmail.com>
2016-12-21 19:38:37 -0600

    initial commit for auto calc

---
+++ AutoCalc/app/src/main/java/com/comp259/acb/autocalc/MainActivity.java
@@ -0,0 +1,13 @@
+package com.comp259.acb.autocalc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
