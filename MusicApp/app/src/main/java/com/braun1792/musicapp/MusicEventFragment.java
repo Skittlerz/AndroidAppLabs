a67f8aed4468c58b3e74ee056619d0fe8c318ae6
Amanda Braun <Ac_br_aun@hotmail.com>
2017-01-05 16:28:44 -0600

    setting up music app
    xmls, strings, and colors

---
+++ MusicApp/app/src/main/java/com/braun1792/musicapp/MusicEventFragment.java
@@ -0,0 +1,19 @@
+package com.braun1792.musicapp;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by braun1792 on 1/5/2017.
+ */
+public class MusicEventFragment extends Fragment {
+
+    public MusicEventFragment(){}
+
+    //public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+
+    //}
+}
---
+++ MusicApp/app/src/main/java/com/braun1792/musicapp/MusicEvents.java
@@ -0,0 +1,22 @@
+package com.braun1792.musicapp;
+
+/**
+ * Created by braun1792 on 1/5/2017.
+ */
+public class MusicEvents {
+
+    static public String[] data = {
+            "6/24 - Spinnerette",
+            "6/25 - Tame Impala",
+            "6/26 - Florence & the Machine",
+            "6/27 - Weezer",
+            "7/22 - Pixies",
+            "7/23 - The Strokes",
+            "7/24 - Tegan & Sara",
+            "7/25 - The Black Keys",
+            "8/04 - Queens of the Stone Age",
+            "8/05 - Alabama Shakes",
+            "8/06 - Adele",
+            "8/07 - Lana Del Rey"
+    };
+}
