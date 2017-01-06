9050ebad8bb47277a39b214339509cd7bdeb1993
Amanda Braun <Ac_br_aun@hotmail.com>
2017-01-05 18:36:19 -0600

    finished music app
    and initial setup of shades app

---
+++ MusicApp/app/src/main/java/com/braun1792/musicapp/MenuActivity.java
@@ -0,0 +1,26 @@
+package com.braun1792.musicapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by acb on 2017-01-05.
+ */
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+    }
+
+    public void musicScreen(View view)
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
--- MusicApp/app/src/main/java/com/braun1792/musicapp/MusicEventFragment.java
+++ MusicApp/app/src/main/java/com/braun1792/musicapp/MusicEventFragment.java
@@ -5,6 +5,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by braun1792 on 1/5/2017.
@@ -13,7 +19,21 @@ public class MusicEventFragment extends Fragment {
 
     public MusicEventFragment(){}
 
-    //public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
 
-    //}
+        //collect data from MusicEvents class
+        String [] eventData = MusicEvents.data;
+        List<String> seasonEvents = new ArrayList<>(Arrays.asList(eventData));
+
+        ArrayAdapter<String> musicEventAdapter = new ArrayAdapter<String>(getActivity(),
+                R.layout.list_item_event, R.id.list_item_event_textView,seasonEvents);
+
+        View rootView = inflater.inflate(R.layout.activity_fragment, container, false);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.lvEvents);
+        listView.setAdapter(musicEventAdapter);
+
+        return rootView;
+
+    }
 }
